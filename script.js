59e5d2aa45f69a21a5473e0a8241812c64dc6780
linnea <linnea.albertsson@gmail.com>
2021-11-16 11:49:29 +0100

    rum 1 = gömda knappar

--- script.js
+++ script.js
@@ -4,19 +4,24 @@ let text = document.querySelector('p');
 
 function main() {
     wakingUp();
+    showBtn3();
     addEventListener();
-   
+
 }
 function addEventListener() {
-
+    
 }
 
 function wakingUp() {
-    document.getElementById('infoField').innerHTML = "Du vaknar till liv. Dina tankar börjar frenetiskt arbeta med att erinra sig händelseförloppet som förde dig hit. VÄNTA. Vart är jag?  Du öppnar långsamt ögonen. Vilket visar sig vara fullkomligt onödigt för det är kolsvart runt omkring dig, med undantag för suddiga konturer av… ja, vadå? Du börjar ta in miljön omkring. Marken under dig är mjuk-, eller nej. Fluffig är ett bättre ord. Marken är fluffig. Vad gör du härnäst?";
-    let btn3 = document.getElementById('btn3');
+    text.innerHTML = "Du vaknar till liv. Dina tankar börjar frenetiskt arbeta med att erinra sig händelseförloppet som förde dig hit. VÄNTA. Vart är jag?  Du öppnar långsamt ögonen. Vilket visar sig vara fullkomligt onödigt för det är kolsvart runt omkring dig, med undantag för suddiga konturer av… ja, vadå? Du börjar ta in miljön omkring. Marken under dig är mjuk-, eller nej. Fluffig är ett bättre ord. Marken är fluffig";
+}
+
+function showBtn3() {
+    setTimeout(function() {
+        btn3.style.display = 'block';
+    }, 2000);
     btn3.innerHTML = "Fortsätt";
-    btn3.onclick = chooseStepTwo();
-   
+    
 }
 
 function chooseStepTwo() {
@@ -37,15 +42,15 @@ function ChocolateFlood() {
     document.getElementById('infoField').innerHTML = "Du närmar dig floden";
     btn1.innerHTML = "Slå tillbaka";
     btn1.onclick = hitBackFunction;
-   
+
 }
 
 function tastingGrass() {
-    
+
 }
 
 function lickingTree() {
-   
+
 }
 
 function hitBackFunction() {
